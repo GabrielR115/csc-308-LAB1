bed60f7f62af50f109abe2d095615d3fc42cb2c6
Gabriel <gabrielrocha101@icloud.com>
2026-01-20 18:27:14 -0800

    step 5 complete

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,7 +36,6 @@ const port = 8000;
 
 app.use(express.json());
 
-
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
@@ -57,5 +56,18 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
