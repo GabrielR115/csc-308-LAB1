564fa0337a9b7670902397866f32e48bd929bf63
Gabriel <gabrielrocha101@icloud.com>
2026-01-20 19:22:33 -0800

    Step 7 done.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,6 +34,13 @@ const users = {
 const app = express();
 const port = 8000;
 
+const findUsersByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
+};
+
+
 app.use(express.json());
 
 app.listen(port, () => {
@@ -50,14 +57,19 @@ const findUserByName = (name) => {
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+  const job = req.query.job;
+
+  if (name !== undefined && job !== undefined) {
+    const result = findUsersByNameAndJob(name, job);
+    res.send({ users_list: result });
+  } else if (name !== undefined) {
+    const result = findUserByName(name);
+    res.send({ users_list: result });
   } else {
     res.send(users);
   }
 });
+
 
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
@@ -81,5 +93,31 @@ app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
+});
+
+
+
+const deleteUserById = (id) => {
+  const index = users["users_list"].findIndex(
+    (user) => user["id"] === id
+  );
+
+  if (index === -1) {
+    return false;
+  }
+
+  users["users_list"].splice(index, 1);
+  return true;
+};
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const deleted = deleteUserById(id);
+
+  if (!deleted) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send();
+  }
 });
 
